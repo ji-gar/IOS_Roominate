37a043d33ff2c901af7701022ef0b7b32a2eff31
Nandini Vithlani <nandinivithlani99@gmail.com>
2026-05-31 16:08:53 +0530

    Initial Commit

---
+++ Roominate/RoominateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoominateApp.swift
+//  Roominate
+//
+//  Created by Nandini Vithlani on 31/05/26.
+//
+
+import SwiftUI
+
+@main
+struct RoominateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
